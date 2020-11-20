737031e4101c0d31f721a7f9814a41972070cdeb
UpperCod <mtrujillo@outlook.cl>
2020-11-20 17:59:36 -0300

    ignore build

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import builtins from "builtin-modules";
 import pkg from "./package.json";
 
 export default {
-    input: ["./src/schema.js"],
+    input: ["./src/index.js", "./src/filter.js", "./src/transform.js"],
     output: {
         dir: "./",
-        format: "./",
+        format: "esm",
         sourcemap: true,
     },
     external: [...builtins, ...Object.keys(pkg.dependencies)],
